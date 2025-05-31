00b3428f86f13ae8bbf5b85071809619593c2556
kp009 <prik4ed@gmail.com>
2025-05-30 19:36:54 -0700

    updated static files and templates

--- azdjango/azdjango/settings.py
+++ azdjango/azdjango/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 import os
 STATIC_URL = 'static/'
+
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STORAGES = {
     "default": {
--- azdjango/core/views.py
+++ azdjango/core/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello from the Core App!")
-    # return render(request, "index.html")
+    #return HttpResponse("Hello from the Core App!")
+    return render(request, "index.html")
