e625c6dd6502b210e22288c1bf5a9dcfe6564665
kp009 <prik4ed@gmail.com>
2025-05-30 22:58:58 -0700

    Added product descriptions to data and template

--- azdjango/core/urls.py
+++ azdjango/core/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from .views import home
+from . import views
+from .views import ProductListCreateView
+
 
 urlpatterns = [
     path('', home, name='home'),
+    path('products/', views.product_list, name='product_list'),
+    path('api/products/', ProductListCreateView.as_view(), name='product-list-create'),
+
 ]
--- azdjango/core/views.py
+++ azdjango/core/views.py
@@ -6,3 +6,28 @@ from django.http import HttpResponse
 def home(request):
     #return HttpResponse("Hello from the Core App!")
     return render(request, "index.html")
+
+from .models import Product
+
+def product_list(request):
+    products = [
+        {
+            'name': 'Laptop',
+            'price': '999.99',
+            'description': 'A high-performance laptop suitable for all your work and gaming needs.'
+        },
+        {
+            'name': 'Keyboard',
+            'price': '49.99',
+            'description': 'A mechanical keyboard with RGB lighting and responsive keys.'
+        }
+    ]
+    return render(request, 'product_list.html', {'products': products})
+
+from .serializers import ProductSerializer
+from rest_framework import generics
+
+
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
